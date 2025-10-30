b16c1880acbc91b004a0d9370f5cae8820d8e298
pedromatta <pedromatta8200@gmail.com>
2025-10-30 09:25:27 -0300

    fix: impedindo erro ao remover os arquivos .class

--- start_client.sh
+++ start_client.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd ./Client
-rm ./RMI/*.class 
+rm -f ./RMI/*.class 
 javac -encoding utf8 ./RMI/*.java
 LC_ALL=C.UTF-8 java RMI.BibliotecaClient
--- start_server.sh
+++ start_server.sh
@@ -2,6 +2,6 @@
 
 cd ./Server
 LOCAL_IP=$(ip route get 1.1.1.1 | awk '/src/ {print $7}')
-rm ./RMI/*.class 
+rm -f ./RMI/*.class 
 javac -encoding utf8 ./RMI/*.java
 LC_ALL=C.UTF-8 java RMI.BibliotecaServer "$LOCAL_IP"
